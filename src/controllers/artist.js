f6d1baa90fd09d4fd707a9e2841161876e50cbe0
Nathan Horne <n.horne90@hotmail.co.uk>
2021-12-21 13:57:55 +0000

    added ability to read artists

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -18,3 +18,16 @@ exports.create = async (req, res) => {
 
   db.close();
 };
+
+exports.read = async (req, res) => {
+  const db = await getDb();
+
+  try {
+    const artists = await db.query('SELECT * FROM Artist');
+
+    res.status(200).json(artists[0]);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  db.close();
+};
